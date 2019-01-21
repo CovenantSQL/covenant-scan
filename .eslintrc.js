79bc1ef3d8c8ca1f1dc83fd69e740bac8a2503ae
foreseaz <foreseazhang@gmail.com>
2019-01-21 16:07:02 +0800

    Add prettier config and lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,25 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        tabWidth: 4,
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        parser: 'vue',
+      },
+    ],
   },
   parserOptions: {
-    parser: "typescript-eslint-parser"
-  }
-};
+    parser: 'typescript-eslint-parser',
+  },
+}
